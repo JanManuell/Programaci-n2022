04c6a9a69e9f0b595bbfb02ba942d12d4eb44e52
JanManuell <68677252+JanManuell@users.noreply.github.com>
2022-10-04 16:06:53 -0500

    Ejercicio2
    Programación en C

---
+++ e2_especificaciones.c
@@ -0,0 +1,17 @@
+/*Ejercicio 2 - Especificación de formato*/
+/*Directivas de procesamiento*/
+#include <stdio.h> /*Libreria entradas-salidas*/
+/*Declaraciones variables globales*/
+//int pin=0;
+/*Función principal*/
+int main ()
+{
+	/*Declaración variables locales*/
+	float x=6.367886, z=8.966532;/*x, z son reales*/
+	int y=16, m=-196; /*y, m son enteros*/
+	/*Instrucciones*/
+	printf("Hola programadores 2022");
+	printf("\n%f %u %f %u",x,y,z,m);//Mostrar variables - especificaciones (%d)
+	printf("\n \"Clase de programacion\"");//
+	//return 0;	
+}
