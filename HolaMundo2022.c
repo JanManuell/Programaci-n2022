5a0a7470e32613ae551161cbd83d27232ff53695
JanManuell <68677252+JanManuell@users.noreply.github.com>
2022-10-04 16:02:31 -0500

    Ejercicio1
    Introducción a C

---
+++ HolaMundo2022.c
@@ -0,0 +1,13 @@
+/*Directivas de procesamiento*/
+#include <stdio.h>/*Librerias*/
+/*Declaraciones variables globales*/
+int pin=0;
+/*Función principal*/
+int main ()
+{
+	/*Declaración variables locales*/
+	float x=6.1;
+	/*Instrucciones*/
+	printf("Hola Clase 2022");
+	//return 0;	
+}
