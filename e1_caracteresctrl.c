9a15e87624769c8495ca85423b114f9c1312a298
JanManuell <68677252+JanManuell@users.noreply.github.com>
2022-10-04 16:04:56 -0500

    Ejercicio1
    Programación en C

---
+++ e1_caracteresctrl.c
@@ -0,0 +1,17 @@
+/*Directivas de procesamiento*/
+#include <stdio.h>/*Librerias*/
+/*Declaraciones variables globales*/
+int pin=0;
+/*Función principal*/
+int main ()
+{
+	/*Declaración variables locales*/
+	float x=6.1;
+	/*Instrucciones*/
+	printf("Hola Clase 2022");
+	printf("\nHola Clase 2022");//Permite pasar a una nueva linea
+	printf("\nX \tY");//Permite tabular horizontalmente
+	printf("\n ALERTA \a");//
+	printf("\n \"Clase de programacion\"");//
+	//return 0;	
+}
